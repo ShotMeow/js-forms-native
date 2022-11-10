0ac15c60647d523937aa974ec9e4ebb052d200f3
shotmeow <shotmeow@gmail.com>
2022-11-10 18:20:30 +0300

    Complete Validation

--- js/script.js
+++ js/script.js
@@ -19,8 +19,72 @@ const getImage = (file) => {
     })
 }
 
-form.addEventListener('submit', (event) => {
+const getErrors = async (data) => {
+    const errors = {
+        companyName: '',
+        title: '',
+        description: '',
+        text: '',
+        image: ''
+    };
+
+    for (const [key, value] of data) {
+        if (key === 'image') {
+            if (value.size === 0 || !value.type.startsWith('image/')) {
+                errors[key] = 'Изображение не опознано';
+                continue;
+            }
+
+            await getImage(value)
+                .then((image) => {
+                    if (image.width < 200 || image.height < 200) {
+                        errors[name] = 'Изображено должно быть минимум 200x200';
+
+                    }
+                })
+            continue;
+        }
+
+        if (value.length === 0) {
+            errors[key] = 'Поле не может быть пустым';
+            continue;
+        }
+
+        switch (key) {
+            case 'title': {
+                if (value.length > 20) {
+                    errors[key] = 'Название должно быть не больше 20 символов';
+                }
+                break;
+            }
+
+            case 'description': {
+                if (value.length > 140) {
+                    errors[key] = 'Подводка должна быть не больше 140 символов';
+                }
+                break;
+            }
+
+            case 'text': {
+                if (value.length < 140) {
+                    errors[key] = 'Текст должна быть не меньше 140 символов';
+                }
+                break;
+            }
+
+            default: {
+                break;
+            }
+        }
+    }
+}
+
+form.addEventListener('submit', async (event) => {
     event.preventDefault();
+
+    const formData = new FormData(form);
+
+    const errors = await getErrors(formData);
 })
 
 fileInput.addEventListener('change', (event) => {
@@ -29,13 +93,13 @@ fileInput.addEventListener('change', (event) => {
     imageContainer.innerHTML = "";
 
     if (!files.length) {
-        return
+        return;
     }
 
     const file = files[0];
 
     if (file.size === 0 || !file.type.startsWith('image/')) {
-        return
+        return;
     }
 
     getImage(file)
